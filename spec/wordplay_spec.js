33b06e78465636546216c250284bb5f3ef947086
quelledanielle <quelledanielle@gmail.com>
2014-07-15 13:00:24 -0400

    Add Wordplay permutations and spec

---
+++ spec/wordplay_spec.js
@@ -0,0 +1,64 @@
+;(function(exports) {
+  var Wordplay = require('../wordplay');
+
+  describe('permutations', function() {
+    var permutations = Wordplay.permutations;
+
+    it('should return the permutations for 0 elements', function() {
+      var a = [];
+      expect(permutations(a)).toEqual([[]]);
+    });
+
+    it('should return the permutations for 1 element', function() {
+      var a = [0];
+      expect(permutations(a)).toEqual([[0]]);
+    });
+
+    it('should return the permutations for 2 elements', function() {
+      var a = [0, 1];
+      var p = [[0, 1], [1, 0]];
+      expect(permutations(a)).toEqual(p);
+    });
+
+    it('should return the permutations for 3 elements', function() {
+      var a = [0, 1, 2];
+      var p = [[0, 1, 2],
+               [0, 2, 1],
+               [1, 2, 0],
+               [1, 0, 2],
+               [2, 0, 1],
+               [2, 1, 0]];
+      expect(permutations(a).sort().join('')).toEqual(p.sort().join(''));
+    });
+
+    it('should return the permutations for 4 elements', function() {
+      var a = [0, 1, 2, 3];
+      var p = [[0, 1, 2, 3],
+               [0, 1, 3, 2],
+               [0, 2, 1, 3],
+               [0, 2, 3, 1],
+               [0, 3, 1, 2],
+               [0, 3, 2, 1],
+               [1, 0, 2, 3],
+               [1, 0, 3, 2],
+               [1, 2, 0, 3],
+               [1, 2, 3, 0],
+               [1, 3, 0, 2],
+               [1, 3, 2, 0],
+               [2, 0, 1, 3],
+               [2, 0, 3, 1],
+               [2, 1, 0, 3],
+               [2, 1, 3, 0],
+               [2, 3, 0, 1],
+               [2, 3, 1, 0],
+               [3, 0, 1, 2],
+               [3, 0, 2, 1],
+               [3, 1, 0, 2],
+               [3, 1, 2, 0],
+               [3, 2, 0, 1],
+               [3, 2, 1, 0]];
+      expect(permutations(a).sort().join('')).toEqual(p.sort().join(''));
+    });
+  });
+
+})(this);
---
+++ wordplay.js
@@ -0,0 +1,49 @@
+var Wordplay = {
+  permutations: function(array) {
+    if (array.length < 2) {
+      return [array];
+    } else if (array.length === 2) {
+      return [array, swapped(array, 0, 1)];
+    } else {
+      return array.reduce(function(result, e, i, a) {
+        // Permute the rest
+        var pRest = Wordplay.permutations(rest(a, i));
+
+        // Append e to each permutation in pRest
+        pElement = pRest.map(function(p) {
+          p.unshift(e);
+          return p;
+        });
+
+        // Add permutations for e to result
+        return result.concat(pElement);
+      }, []);
+    }
+  }
+};
+
+var copy = function(array) {
+  return array.slice(0);
+}
+
+var rest = function(array, i) {
+  ensureIndexInBounds(array, i);
+  return array.slice(0, i).concat(array.slice(i + 1));
+}
+
+var swapped = function(array, i, j) {
+  ensureIndexInBounds(array, i);
+  ensureIndexInBounds(array, j);
+
+  var a = copy(array);
+  var t = a[i];
+  a[i] = a[j];
+  a[j] = t;
+  return a;
+}
+
+var ensureIndexInBounds = function(array, i) {
+  if (i < 0 || i >= array.length) throw new Error('Index out of bounds');
+}
+
+module.exports = Wordplay;
