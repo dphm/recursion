1ed2b2e134c7022231d75b0f163c8b9d58dd246a
quelledanielle <quelledanielle@gmail.com>
2014-07-17 17:15:55 -0400

    Move valid Scrabble words to function

--- wordplay/scrabble_wordplay.js
+++ wordplay/scrabble_wordplay.js
@@ -3,14 +3,18 @@ var wordplay = require('./wordplay');
 var Dictionary = require('./dictionary').Dictionary;
 var hasWord = require('./dictionary').hasWord;
 
-var seriesOfTiles = function(hand) {
+function seriesOfTiles(hand) {
   return wordplay.seriesOfLetters(hand.letters());
 };
+
+function validWords(Dictionary, hand) {
+  return seriesOfTiles(hand).filter(function(series) {
+    var s = series.join('').toLowerCase();
+    return hasWord(Dictionary, s);
+  });
+}
 
 var bag = new Scrabble.Bag();
 var hand = new Scrabble.Hand(bag);
 console.log(hand.letters());
-seriesOfTiles(hand).forEach(function(series) {
-  var s = series.join('').toLowerCase();
-  if (hasWord(Dictionary, s)) console.log(s);
-});
+console.log(validWords(Dictionary, hand));
